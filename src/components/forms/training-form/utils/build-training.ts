c4feea75329b78dcefc35f246ba87c0e039370ec
Niv Zelber <nivznivz0@gamil.com>
2020-07-21 21:18:45 +0300

    update build training method to handle rounds parameter

--- src/components/forms/training-form/utils/build-training.ts
+++ src/components/forms/training-form/utils/build-training.ts
@@ -3,9 +3,16 @@ import { TrainingFormOutput } from "../types";
 import { defaultTimeValue } from "../constants";
 
 export const buildTraining = (trainingOutput: TrainingFormOutput): Training =>
-  trainingOutput.map(
-    ({ workTime = defaultTimeValue(), breakTime = defaultTimeValue() }) => ({
+  trainingOutput
+    .map(
+      ({
+        workTime = defaultTimeValue(),
+        breakTime = defaultTimeValue(),
+        rounds = "1"
+      }) => [...Array(parseInt(rounds))].map(() => [workTime, breakTime])
+    )
+    .flat()
+    .map(([workTime, breakTime]) => ({
       workTime: workTime.valueOf(),
       breakTime: breakTime.valueOf()
-    })
-  );
+    }));
